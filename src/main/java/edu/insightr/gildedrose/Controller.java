2874e31693f7c9f9d1379ad30a55545cc86530f7
AmineMiliani <miliani.amine@orange.fr>
2018-11-05 15:40:39 +0100

    Start Controller

---
+++ src/main/java/edu/insightr/gildedrose/Controller.java
@@ -0,0 +1,35 @@
+package edu.insightr.gildedrose;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+
+public class Controller {
+
+    @FXML
+    private ListView<?> listViewItems;
+
+    @FXML
+    private Button buttonUpdate;
+
+    @FXML
+    private Button buttonAdd;
+
+    @FXML
+    private ListView<?> listViewTestResult;
+
+    @FXML
+    private Button buttonTest;
+
+    @FXML
+    private Button buttonDelete;
+
+    @FXML
+    private Button buttonEdit;
+
+    @FXML
+    private Button buttonSave;
+
+    @FXML
+    private Button buttonCancel;
+
+}
