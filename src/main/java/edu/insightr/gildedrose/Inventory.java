a68ed0e146fb0e149bae429f87ac546e1dd546dc
Im2Slow <adrien.kerisit@gmail.com>
2018-11-19 14:14:07 +0100

    Modified updateMethod, added JSON file

--- src/main/java/edu/insightr/gildedrose/Inventory.java
+++ src/main/java/edu/insightr/gildedrose/Inventory.java
@@ -17,11 +17,9 @@ public class Inventory {
                 new Item("+5 Dexterity Vest", 10, 20),
                 new Item("Aged Brie", 2, 0),
                 new Item("Elixir of the Mongoose", 5, 7),
-                new Item("Sulfuras, Hand of Ragnaros", 0, 80),
+                new Item("Legendary Sulfuras, Hand of Ragnaros", 0, 80),
                 new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
                 new Item("Conjured Mana Cake", 1, 16),
-                new Item("",0,0),
-                new Item("",0,0),
 
         };
 
@@ -55,13 +53,13 @@ public class Inventory {
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
             if(items[i] != null) {
-                if (items[i].getName() != "Aged Brie"
-                        && items[i].getName() != "Backstage passes to a TAFKAL80ETC concert") {
+                if (!(items[i].getName().contains("Aged")
+                        && items[i].getName().contains("concert"))) {
                     if (items[i].getQuality() > 0) {
-                        if (items[i].getName() != "Sulfuras, Hand of Ragnaros") {
+                        if (!items[i].getName().contains("Legendary")) {
                             //items[i].setQuality(items[i].getQuality() - 1);
 
-                            if (items[i].getName().contains("Conjured")) {
+                            if (!items[i].getName().contains("Conjured")) {
                                 items[i].setQuality(items[i].getQuality() - 2);
                             } else {
                                 items[i].setQuality(items[i].getQuality() - 1);
@@ -72,7 +70,7 @@ public class Inventory {
                     if (items[i].getQuality() < 50) {
                         items[i].setQuality(items[i].getQuality() + 1);
 
-                        if (items[i].getName() == "Backstage passes to a TAFKAL80ETC concert") {
+                        if (!items[i].getName().contains("concert")) {
                             if (items[i].getSellIn() < 11) {
                                 if (items[i].getQuality() < 50) {
                                     items[i].setQuality(items[i].getQuality() + 1);
@@ -88,15 +86,15 @@ public class Inventory {
                     }
                 }
 
-                if (items[i].getName() != "Sulfuras, Hand of Ragnaros") {
+                if (!items[i].getName().contains("Legendary")) {
                     items[i].setSellIn(items[i].getSellIn() - 1);
                 }
 
                 if (items[i].getSellIn() < 0) {
-                    if (items[i].getName() != "Aged Brie") {
-                        if (items[i].getName() != "Backstage passes to a TAFKAL80ETC concert") {
+                    if (!items[i].getName().contains("Aged")) {
+                        if (!items[i].getName().contains("concert")) {
                             if (items[i].getQuality() > 0) {
-                                if (items[i].getName() != "Sulfuras, Hand of Ragnaros") {
+                                if (!items[i].getName().contains("Legendary")) {
 
                                     if (items[i].getName().contains("Conjured")) {
                                         items[i].setQuality(items[i].getQuality() - 2);
