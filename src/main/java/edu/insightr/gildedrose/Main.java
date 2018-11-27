886035d63756bc9372fbd5c22f25686217e94da7
AmineMiliani <miliani.amine@orange.fr>
2018-11-27 16:14:44 +0100

    Resize window and change pieChart colors

--- src/main/java/edu/insightr/gildedrose/Main.java
+++ src/main/java/edu/insightr/gildedrose/Main.java
@@ -6,6 +6,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.Objects;
+
 public class Main extends Application {
 
     public static void main(String[] args) {
@@ -16,9 +18,9 @@ public class Main extends Application {
     public void start(Stage primaryStage){
         try {
             // TODO (PBZ) : using pixel size into your fxml is not a good idea (no responsive, no decoration isolation)
-            Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("Inventory.fxml"));
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("Inventory.fxml")));
             primaryStage.setTitle("Main Window");
-            Scene scene = new Scene(root, 650, 500);
+            Scene scene = new Scene(root, 1000, 400);
             scene.getStylesheets().add("styles.css");
             primaryStage.setScene(scene);
             primaryStage.show();
